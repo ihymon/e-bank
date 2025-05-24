7ab621fc06d4d8d4b290a2d2384462ace9d9c424
MarkivNazar <nazarmn2008@gmail.com>
2025-05-24 15:34:33 +0300

    Create Card

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './Card.css'
+
+export default function Card() {
+  return (
+    <div className="card">
+
+        <div className="Mini-Card">
+
+      <h2>Card</h2>
+     <input type="number" className='number'  placeholder='0434 3535 3535 3535'/>
+</div>
+      
+    </div>
+  )
+}
