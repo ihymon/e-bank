12a9d303e3ae5706e4ad4f9be26fea52876af0db
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-05-24 15:46:56 +0300

    create subscribe screen

--- src/components/Subscribe/Subscribe.jsx
+++ src/components/Subscribe/Subscribe.jsx
@@ -6,7 +6,7 @@ export default function Subscribe() {
             <h1>Subscribe</h1>
             <p>Subscribe to our newsletter to get the latest news!</p>
             <div className='subscribe-form'>
-                <input type="text" placeholder='Enter your email' />
+                <input type="text" placeholder='Enter your email' className='subscribe-input' />
                 <button>Subscribe</button>
             </div>
         </div>
