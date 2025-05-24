3771161a77c0cc66977ffceefa913a538c74fd64
Sviatoslav <107312413+Sviatik17@users.noreply.github.com>
2025-05-24 15:45:51 +0300

    add feedback

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Feedback.css'
+
+export default function Feedback() {
+  return (
+    <div>
+        <div className="Photo"></div>
+        <p className="Username">taras</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod repudiandae reprehenderit quidem natus sapiente itaque, officia, pariatur officiis similique dicta aliquid ipsa! Dolore sapiente similique exercitationem? Alias explicabo odio eum, non voluptatum harum esse officiis in blanditiis ullam rem? Alias distinctio iure id repudiandae quia deleniti fugit, explicabo eum quod!</p>
+    </div>
+  )
+}
