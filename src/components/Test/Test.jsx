5edfc6bf9812ca871c9b29deea6ec77f034d893f
Vitaliy Hymon <vitaliyhymon@gmail.com>
2025-05-24 16:00:35 +0300

    change btn

---
+++ src/components/Test/Test.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Test() {
+  return (
+    <div>
+        <h2>Test</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque minima earum possimus voluptatibus cupiditate ducimus, voluptatum consequuntur! Culpa officiis unde illo doloribus hic tenetur exercitationem cumque repellendus omnis obcaecati? Reprehenderit, incidunt ab. Dolorum ducimus totam incidunt vel, repudiandae quidem amet praesentium quasi similique, voluptatum delectus blanditiis unde saepe eligendi maxime optio labore id dolores? Quae, officia quibusdam rerum excepturi laudantium nam rem fugit aliquam adipisci aspernatur at illo facilis illum sit iste commodi delectus temporibus qui, voluptatum minima unde maxime inventore error placeat. Aut quo nihil neque obcaecati? Alias perferendis ea sint ipsa dolore commodi accusantium nobis, minima quasi quod?</p>
+        <button>subscribe</button>
+    </div>
+  )
+}
